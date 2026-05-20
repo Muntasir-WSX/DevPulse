26d6c04c9db151521bdc2d56c6a07979f33e5629
Muntasir <alimuntasir2001@gmail.com>
2026-05-21 00:43:31 +0600

    test routes done

--- src/server.ts
+++ src/server.ts
@@ -5,13 +5,41 @@ const server : Server = createServer((req : IncomingMessage, res: ServerResponse
     const url = req.url
     const method = req.method
 
-    if ()
+    if ( url === "/" && method === "GET" ) {
+        // console.log("Received a GET request to the root path");
+
+        res.writeHead(200, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({ message: "Hello, World! This is the root path." }));
+    }
+
+else if (url?.startsWith ("/api/users"))
+{
+    res.writeHead(200, {"Content-Type": "application/json"});
+    res.end(JSON.stringify({ message: "Welcome to the Users API!",
+            users: [
+                { id: 1, name: "Muntasir" },
+                { id: 2, name: "Tanjim" }
+            ]
+        }));
+}
+
+
+
+
+
+    else {
+        res.writeHead(404, {"Content-Type": "application/json"});
+        res.end(JSON.stringify({ error: "404 Not Found: The requested resource was not found on this server." }));
+    }
+
+
+    
 });
 
 
 server.listen(5000, ()=> 
     
-    console.log("Server is running on port 3000"
+    console.log("Server is running on port 5000"
 
     )
 );
