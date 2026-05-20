a1a13c09e75227b737e2f6cc8d3f834d5cfe9288
Muntasir <alimuntasir2001@gmail.com>
2026-05-21 00:05:05 +0600

    Primary Setup Done

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import { createServer, IncomingMessage, Server, ServerResponse } from "http";
+
+const server : Server = createServer((req : IncomingMessage, res: ServerResponse) => {
+ 
+    const url = req.url
+    const method = req.method
+
+    if ()
+});
+
+
+server.listen(5000, ()=> 
+    
+    console.log("Server is running on port 3000"
+
+    )
+);
